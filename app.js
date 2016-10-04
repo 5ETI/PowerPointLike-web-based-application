5bcc2a78d98f62f7b93bbdf81da63249e93b2cff
bertrandp <bertrand.pestre@hotmail.fr>
2016-10-04 13:47:06 +0200

    valid node step 2

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-console.log("It works !!\n\n");
+"use strict";
 
-var express = require("express");
-var app = express();
+console.log("It works !");
 
-var http = require("http");
+var  express  =  require("express");
+var  app  =  express();
+var  http  =  require("http");
+var  CONFIG  =  require("./config.json");
+process.env.CONFIG  =  JSON.stringify(CONFIG);
+// init server
+var  server  =  http.createServer(app);
+server.listen(CONFIG.port);
+
+var  defaultRoute  =  require("./app/routes/default.route.js");
+app.use(defaultRoute);
+
+/*// #2
+app.get("/",  function (request, response) {
+    response.send("It works !");
+});
+// #3
+app.use( function (request, response, cb) {
+    response.send("It works !");
+    cb();
+});*/
+
+var  path  =  require("path");
+
+app.use("/index",express.static(path.join(__dirname, "/public")));
